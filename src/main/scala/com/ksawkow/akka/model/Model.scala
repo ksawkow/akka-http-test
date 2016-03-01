db99c36e646a4d388b6031998394b3b503deb204
Sawkow, Krzysztof <krzysztof.sawkow@sap.com>
2016-03-01 22:39:51 +0100

    JsonProtocol minor clean-up

--- src/main/scala/com/ksawkow/akka/model/Model.scala
+++ src/main/scala/com/ksawkow/akka/model/Model.scala
@@ -7,10 +7,5 @@ case class CompanyDetails(name: String, description: String)
 object JsonProtocol extends JsonProtocol
 
 trait JsonProtocol extends DefaultJsonProtocol {
-
-  //  implicit val mongoErrorFormat = jsonFormat1(MongoError)
-  //  implicit val mongoAlreadyExistsFormat = jsonFormat1(MongoAlreadyExists)
-  //  implicit val mongoNotFoundFormat = jsonFormat1(MongoNotFound)
-
   implicit val companyDetailsFormat = jsonFormat2(CompanyDetails)
 }
